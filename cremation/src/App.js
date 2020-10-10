b6df1de5bf3fbae32ddbe30f8ce4cc75d34d17dd
AmaaniGoose <amanullah1707@gmail.com>
2020-10-10 12:32:46 +0530

    Responsive design fixesd

--- cremation/src/App.js
+++ cremation/src/App.js
@@ -4,24 +4,28 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Image src="./blueflame.gif" roundedCircle />
-            <div class="row">
-              <h1 class="display-1">
-              <p>
-                  Blue Flames of Cremation
+  <div className="App">
+    <header className="App-header">
+      <Image src="./blueflame.gif" roundedCircle="roundedCircle"/>
+        <div class="row">
+          <div class="col-12 col-sm-12 ">
+            <h1 class="display-1">
+              <p class="text-center">
+                Blue Flames of Cremation
               </p>
-              </h1>
-              </div>
-                <div class="row">
-                  <p class="lead">
-                    Coming Soon
-                  </p>
-                </div>
-      </header>
-    </div>
-  );
+            </h1>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12 col-sm-12 align-items-center">
+            <p class="lead">
+              Coming Soon
+            </p>
+          </div>
+        </div>
+    </header>
+  </div>
+);
 }
 
 export default App;
