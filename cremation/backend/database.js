69bcdb56d31e3e36b9ab55de46d47a7982453837
AmaaniGoose <55527135+AmaaniGoose@users.noreply.github.com>
2020-10-10 18:58:45 +0530

    Update database.js

--- cremation/backend/database.js
+++ cremation/backend/database.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const connection = "fill";
 mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
     .then(() => console.log("Database Connected Successfully"))
     .catch(err => console.log(err));
