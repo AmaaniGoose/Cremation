f60eb92663254fa794988bb9296ac9a3040d9cf3
AmaaniGoose <amanullah1707@gmail.com>
2020-10-10 11:29:48 +0530

    Added production page

--- cremation/src/App.js
+++ cremation/src/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import Image from 'react-bootstrap/Image'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Image src="./blueflame.gif" roundedCircle />
+            <div class="row">
+              <h1 class="display-1">
+              <p>
+                  Blue Flames of Cremation
+              </p>
+              </h1>
+              </div>
+                <div class="row">
+                  <p class="lead">
+                    Coming Soon
+                  </p>
+                </div>
       </header>
     </div>
   );
