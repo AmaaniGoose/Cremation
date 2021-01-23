196c67ead5c1777723a4bca5e35bbbf211b2a57f
AmaaniGoose <amanullah1707@gmail.com>
2021-01-23 23:04:41 +0530

    WORKING

--- cremation/src/components/DeathComponent.js
+++ cremation/src/components/DeathComponent.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 class Death extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            result: {}
+        }
     }
+
+    componentDidMount() {
+        axios.get('/api/users')
+            .then((response) => {
+                console.log(response.data[response.data.length-1]);
+                console.log(response.status);
+                console.log(response.statusText);
+                console.log(response.headers);
+                console.log(response.config);
+
+                this.setState({result : response.data[response.data.length-1]});
+            });
+
+     }
     render() {
         return (
             <div class="container vertical center">
@@ -17,8 +35,8 @@ class Death extends Component {
                         <div class="card text-white bg-dark mb-3">
                             <div class="card-header">In Loving Memory Of</div>
                             <div class="card-body">
-                                <h5 class="card-title">{this.props.formValues["cremate"]}</h5>
-                                <p class="card-text">Remembered by {this.props.formValues["name"]}</p>
+                                <h5 class="card-title">{this.state.result["cremate"]}</h5>
+                                <p class="card-text">Remembered by {this.state.result["name"]}</p>
                             </div>
                         </div>
                         {"\n"}
